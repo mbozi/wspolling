0902933675e0df30de1ae16402e01d5921cd5a0c
DAVE <dave.bryant1@ntlworld.com>
2020-03-16 12:24:11 +0000

    0001

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PollingComponent } from './polling/polling.component';
@@ -17,7 +17,8 @@ import { PollingService } from './shared/polling.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [PollingService],
   bootstrap: [AppComponent]
